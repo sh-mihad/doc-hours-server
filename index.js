cad71eefe6bb49a9fd4b3a6fccc5625ba3753b54
sh-mihad <sh.mihad.cse@gmail.com>
2022-11-09 01:05:27 +0600

    sigle data send to ui

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -21,12 +22,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run (){
    try{
     const serviceCollection = client.db("service-project").collection("Services");
-
+    
+    // Service post
      app.post("/service", async(req,res)=>{
       const service = req.body;
      const result = await serviceCollection.insertOne(service);
      res.send(result)
      })
+
+     // getting service for home page
+     app.get("/3service",async(req,res)=>{
+        const query = {}
+        const cursor = serviceCollection.find(query).limit(3)
+        const result = await cursor.toArray()
+        res.send(result)
+     })
+
+     // get all service 
+     app.get("/services",async(req,res)=>{
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const result = await cursor.toArray()
+        res.send(result)
+     })
+
+     // get single service for deatils page
+    app.get("/services/:id", async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)}
+        const result = await serviceCollection.findOne(query);
+        res.send(result)
+    })
      
    }
    finally {
